bd13713e739bf3bed648784d7aff9a1aebea1025
OliverGaskill <gaskilloliver@gmail.com>
2025-10-17 21:11:12 +0200

    else if and if statements

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,24 @@ import java.util.Scanner;
  class Main {
      static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
-
-        System.out.print("Enter adjective (description) ");
-        adjective1 = scanner.nextLine();
-        System.out.print("Enter a noun (Animal or Person) ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter adjective (description) ");
-        adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb ending with -ing (Action) ");
-        verb1 = scanner.nextLine();
-        System.out.print("Enter a adjective (description) ");
-        adjective3 = scanner.nextLine();
 
 
-        System.out.println("Today i went to a " + adjective1 + " zoo.");
-        System.out.println("In a exhibit i saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 + "!");
+         Scanner sc = new Scanner(System.in);
 
-        scanner.close();
+         System.out.print("Enter age: ");
+         int age = sc.nextInt();
+
+         if(age >= 18 ){
+             System.out.println("An adult doofus");
+
+         } else if (age <= 0) {
+             System.out.println("ain't born");
+
+
+         } else{
+             System.out.println("ur a kid");
+         }
+
+
     }
 }
