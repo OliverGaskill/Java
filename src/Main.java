af9a48054526e22f8a398b82c6bf566b94d182c2
OliverGaskill <gaskilloliver@gmail.com>
2025-10-17 12:18:50 +0200

    mad libs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+import java.util.Scanner;
+
+ class Main {
+     static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.print("Enter adjective (description) ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun (Animal or Person) ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter adjective (description) ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb ending with -ing (Action) ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter a adjective (description) ");
+        adjective3 = scanner.nextLine();
+
+
+        System.out.println("Today i went to a " + adjective1 + " zoo.");
+        System.out.println("In a exhibit i saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+        scanner.close();
     }
 }
