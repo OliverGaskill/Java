dfec9a6360097df37b4c4fda143236ad6aac32f6
OliverGaskill <gaskilloliver@gmail.com>
2025-10-22 14:37:33 +0200

    sdh

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,13 @@ import java.util.Scanner;
  class Main {
      static void main(String[] args) {
 
-         Scanner scanner = new Scanner(System.in);
+
+         String name = "Oliver Gaskill";
+
+         int length = name.length();
+
+         System.out.println(length);
 
 
-         double principal;
-         double rate;
-         int timesCompounded;
-         int years;
-         double amount;
-
-
-         System.out.print("Enter the principal amount: ");
-         principal = scanner.nextDouble();
-
-         System.out.print("Enter the interest rate (in %): ");
-         rate = scanner.nextDouble() / 100;
-
-         System.out.print("Enter the # of times compounded per year: ");
-         timesCompounded = scanner.nextInt();
-
-         System.out.print("Enter the number of years: ");
-         years = scanner.nextInt();
-
-         amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
-
-         System.out.printf("The amount after %d year(s) is SEK%.2f", years, amount);
-
-
-         scanner.close();
     }
 }
