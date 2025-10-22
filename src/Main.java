e3f628e00909139ec33367adfd4f9c8172256bbc
OliverGaskill <gaskilloliver@gmail.com>
2025-10-22 16:33:27 +0200

    email slicer with substring

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-
 import java.util.Scanner;
-
 
  class Main {
      static void main(String[] args) {
 
 
-         String name = "Oliver Gaskill";
+         Scanner scanner = new Scanner(System.in);
+         String email;
+         String username;
+         String domain;
 
-         int length = name.length();
 
-         System.out.println(length);
+         System.out.println("Enter your email: ");
+
+         email = scanner.nextLine();
+
+         if (email.contains("@")){
+             username = email.substring(0, email.indexOf("@"));
+             domain = email.substring(email.indexOf("@") + 1);
+
+             System.out.println(username);
+             System.out.println(domain);
+         }
+         else {
+             System.out.println("Emails must contain @");
+         }
 
 
+         scanner.close();
     }
 }
